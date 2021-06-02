5378796b86e26d0d6e20f64516d44071d4f2560d
Rittick Ghosh <78947669+rittick98@users.noreply.github.com>
2021-06-02 09:35:57 +0530

    custom demo

---
+++ DBHandler.java
@@ -0,0 +1,11 @@
+import java.sql.Connection;
+
+public class DBHandler {
+
+	//Write the required business logic as expected in the question description
+	public Connection establishConnection() {
+
+		//fill the code
+
+	}
+}
---
+++ RainfallReport.java
@@ -0,0 +1,25 @@
+import java.util.List;
+
+public class RainfallReport {
+
+	//Write the required business logic as expected in the question description
+	public List<AnnualRainfall> generateRainfallReport(String filePath) {
+	    
+		//fill the code
+		
+	}
+	
+	public List<AnnualRainfall>  findMaximumRainfallCities() {
+	
+		//fill the code
+		
+	}
+	
+	
+	public boolean validate(String cityPincode) throws InvalidCityPincodeException {
+	
+		//fill the code
+    		
+	}
+
+}
